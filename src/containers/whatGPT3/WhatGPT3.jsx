e9ac760908ff960d02212ce58cc977b1ddfaccb1
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-01-21 10:12:40 +0530

    id added in whatGPT3

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,7 +4,7 @@ import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgpt3 section_margin">
+    <div className="gpt3__whatgpt3 section_margin" id="wgpt3">
       <div className="gpt3__whatgpt3-feature">
         <Feature
           title="What is GPT-3"
